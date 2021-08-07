debb1caa9bf779a5dbacf21a1ed7b546e1c739b3
Danni Wilton <danniwilton@gmail.com>
2021-08-07 22:17:32 +0800

    added scripts

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -53,11 +53,6 @@ function Navbar() {
                             Gallery
                         </Link>
                     </li>
-                    <li className= 'nav-item'>
-                        <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
-                            Contact
-                        </Link>
-                    </li>
                 </ul>
                 {button && <Button buttonStyle='btn--outline'>CONTACT</Button>}
 
