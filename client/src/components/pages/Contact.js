4853108a44fd020cd9b0c1898b0af064445efcf4
Danni Wilton <danniwilton@gmail.com>
2021-08-15 20:08:53 +0800

    removed unused dependencies

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -1,24 +1,17 @@
 import React, { useState } from 'react';
 import './contact.css';
-
-// Here we import a helper function that will check if the email is valid
 import { validateEmail } from '../../utils/helpers';
 
 function Form() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
   const [email, setEmail] = useState('');
   const [contactName, setContactName] = useState('');
   const [inputDescription, setInputDescription] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
-
-    // Based on the input type, we set the state of either email, username, and password
     if (inputType === 'email') {
       setEmail(inputValue);
     } else if (inputType === 'contactName') {
@@ -29,19 +22,12 @@ function Form() {
   };
 
   const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
-
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
     if (!validateEmail(email) ) {
       setErrorMessage('Email is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
       return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
     }
     alert(`Thank you for your enquiry ${contactName}, Skull Stylist will respond very soon.`);
-
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
     setContactName('');
     setEmail('');
     setInputDescription('');
@@ -50,7 +36,7 @@ function Form() {
 
   return (
     <div className= "form-box" >
-      
+
       <form className="form">
       <input className="input-small"
           value={contactName}
@@ -86,5 +72,51 @@ function Form() {
     </div>
   );
 }
+// const express = require("express");
+// const router = express.Router();
+// const cors = require("cors");
+// const nodemailer = require("nodemailer");
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use("/", router)
+
+
+// const contactEmail = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: "skull.stylist.email@gmail.com",
+//     pass: "JoshuaHomme21",
+//   },
+// });
+
+// contactEmail.verify((error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Ready to Send");
+//   }
+// });
+// router.post("/contact", (req, res) => {
+//   const name = req.body.name;
+//   const email = req.body.email;
+//   const message = req.body.message; 
+//   const mail = {
+//     from: name,
+//     to: "skull.stylist.email@gmail.com",
+//     subject: "Contact Form Submission",
+//     html: `<p>Name: ${name}</p>
+//            <p>Email: ${email}</p>
+//            <p>Message: ${message}</p>`,
+//   };
+//   contactEmail.sendMail(mail, (error) => {
+//     if (error) {
+//       res.json({ status: "ERROR" });
+//     } else {
+//       res.json({ status: "Message Sent" });
+//     }
+//   });
+// });
+
 
 export default Form;
