ec31ab2af7a4a9f275fad5fb396d02b26faea80f
Danni Wilton <danniwilton@gmail.com>
2021-08-16 16:41:56 +0800

    shop items in shop

--- client/src/components/pages/Shop.js
+++ client/src/components/pages/Shop.js
@@ -5,21 +5,27 @@ import { QUERY_SHOPDATA } from '../../utils/queries';
 export default function Shop() {
     // query/grab data to display in the DOM
     const { loading, data } = useQuery(QUERY_SHOPDATA);
+    const dataArray = data?.getShopItems;
+    // const products = data?.products || [];
+    console.log(useQuery)
+    console.log("products test", data)
     return (
-        <div>
+        <div style={{backgroundImage: 'url(../rockywall/rockywall.png)'}}>
             
-
             {loading && (
-                <h1>Laoding</h1>
+                <h1>Loading...</h1>
             )}
 
             {!loading && (
                 
                 <ul>
-                    <li></li>
+                    {dataArray.map((item)=>{
+                        return (<li><img style={{margin: '15px', padding: '25px', justifyContent: 'center'}} src={item.image} alt={item.name} /><p style={{margin:'10px'}}>{item.name}{item.price}</p><p>{item.description}</p></li>)
+                    })}
                 </ul>
 
             )}
+
 
 
 
