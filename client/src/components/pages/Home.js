1c3c4902dd51d78a914f2e5058284c36169197b1
Danni Wilton <danniwilton@gmail.com>
2021-09-08 14:07:31 +0800

    styled shop

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import '../../App.css';
 import LandingPage from '../LandingPage';
-import Gallery from '../pages/Gallery'
 
 function Home () {
     return (
         <>
             <LandingPage />
-            <Gallery />
         </>
     )
 }
--- client/src/components/pages/Shop.js
+++ client/src/components/pages/Shop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_SHOPDATA } from '../../utils/queries';
+import '../pages/shop.css';
 
 export default function Shop() {
     // query/grab data to display in the DOM
@@ -10,7 +11,7 @@ export default function Shop() {
     console.log(useQuery)
     console.log("products test", data)
     return (
-        <div style={{backgroundImage: 'url(../rockywall/rockywall.png)'}}>
+        <div style={{backgroundImage: "url(../../rockywall/rockywall.png", backgroundColor: "lightsteelblue"}}>
             
             {loading && (
                 <h1>Loading...</h1>
@@ -20,7 +21,7 @@ export default function Shop() {
                 
                 <ul>
                     {dataArray.map((item)=>{
-                        return (<li><img style={{margin: '15px', padding: '25px', justifyContent: 'center'}} src={item.image} alt={item.name} /><p style={{margin:'10px'}}>{item.name}{item.price}</p><p>{item.description}</p></li>)
+                        return (<li style={{marginLeft: '155px'}}><img style={{margin: '25px', justifyContent: 'center'}} src={item.image} alt={item.name} /><p style={{margin:'15px'}}>{item.name}{item.price}</p><p>{item.description}</p></li>)
                     })}
                 </ul>
 
